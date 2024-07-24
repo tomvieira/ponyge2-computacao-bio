ed13caf61fd3dd70f6fd491c35de7cf09b18c326
Wellington Vieira <tomvieira@outlook.com>
2024-07-24 17:17:31 -0300

    normlaize

--- datasets/process_wellington_china.py
+++ datasets/process_wellington_china.py
@@ -1,3 +1,4 @@
+from sklearn import preprocessing
 import numpy as np
 import pandas as pd
 
@@ -13,17 +14,21 @@ filename = "china_gdp_wellington.csv"
 
 urllib.request.urlretrieve(url, filename)
 
-data = np.genfromtxt(filename, delimiter=",")
+data = np.genfromtxt(filename,  skip_header=1, delimiter=",")
 
 np.random.seed(0)
 np.random.shuffle(data)
 fronteira = round(data.shape[0] * 0.8)
+scaler = preprocessing.MinMaxScaler()
+data = scaler.fit_transform(data)
 
 train = data[:fronteira]
 test = data[fronteira:]
-np.savetxt("China-Train.csv", train, delimiter=" ", header="x0 response", fmt="%.6f")
-np.savetxt("China-Test.csv", test, delimiter=" ", header="x0 response", fmt="%.6f")
+np.savetxt("China-Train.csv", train, delimiter=" ",
+           header="x0 response", fmt="%.6f")
+np.savetxt("China-Test.csv", test, delimiter=" ",
+           header="x0 response", fmt="%.6f")
 # Normalizando para melhorar os resultados
-#scaler = preprocessing.MinMaxScaler()
-#df['Year'] = scaler.fit_transform(df[['Year']])
-#df['Value'] = scaler.fit_transform(df[['Value']])
+# scaler = preprocessing.MinMaxScaler()
+# df['Year'] = scaler.fit_transform(df[['Year']])
+# df['Value'] = scaler.fit_transform(df[['Value']])
